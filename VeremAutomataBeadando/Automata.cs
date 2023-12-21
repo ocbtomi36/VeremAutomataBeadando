dd44432b248cb663e69e7f7b2ad0b708f828679d
ocbtomi36 <ocbtomi36@outlook.hu>
2023-12-21 01:25:30 +0100

    Add project files.

---
+++ VeremAutomataBeadando/Automata.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace VeremAutomataBeadando
+{
+    internal class Automata
+    {
+        private int cikluslépteto = 0;
+        private int karakterPozicio = 0;
+        private string szalag;
+        private bool elfogad = false;
+        private bool error = false;
+        private Stack<string> stack = new Stack<string>();
+        private string[,] szabalyMatrix = new string[12, 7];
+        private string alkalmazottSzabalyok = string.Empty;
+
+
+        public Automata(string _input,string[,] _szabalyMatrix) 
+        { 
+            this.szalag = $"{_input}#";
+            this.szabalyMatrix = _szabalyMatrix;
+            this.stack.Push("#");
+            this.stack.Push("E");
+        }
+
+        public string VeremTeto()
+        {
+            return this.stack.Peek();
+        }
+        public string Elemzes()
+        {
+            string eredmeny = string.Empty;
+            int oszlopIndex = 0;
+            int sorIndex = 0;
+            string szabaly = string.Empty;
+            string pattern = @"^[i(]"; // Regex minta az első karakterre
+
+            Regex regex = new Regex(pattern);
+            string input = szalag;
+
+            if (regex.IsMatch(input))
+            {
+                elfogad = false;
+            }
+            else
+            {
+                eredmeny = "Az első karakter nem jó!";
+                error = true;
+            }
+            while (elfogad != true && error != true)
+            {
+                
+                //megkeresi, hogy hol van az aktuális szabály a táblázatban
+                for (int j = 0; j < this.szabalyMatrix.GetLength(1); j++)
+                {
+                    if (this.szabalyMatrix[0, j] == szalag[karakterPozicio].ToString())
+                    {
+                        oszlopIndex = j;
+                    }
+                }
+                for (int j = 0; j < this.szabalyMatrix.GetLength(0); j++)
+                {
+                    if (this.szabalyMatrix[j, 0] == this.stack.Peek())
+                    {
+                        sorIndex = j;
+                    }
+                }
+                stack.Pop();
+                // mátrixból kiveszi az aktuális körben a szabályt és feldarabolja karakterekre
+                if (this.szabalyMatrix[sorIndex,oszlopIndex].Contains("|"))
+                {
+                    string[] matrixSzabalySeged = this.szabalyMatrix[sorIndex, oszlopIndex].Split('|');
+                    if(matrixSzabalySeged.Length != 1)
+                    {
+                        switch (matrixSzabalySeged.Length)
+                        {
+                            case 2:
+                                if (matrixSzabalySeged[0] == "eps")
+                                {
+                                    alkalmazottSzabalyok += matrixSzabalySeged[1];
+                                }
+                                else if (matrixSzabalySeged[0] == "(E)")
+                                {
+                                    string seged = matrixSzabalySeged[0];
+                                    stack.Push(seged[2].ToString());
+                                    stack.Push(seged[1].ToString());
+                                    stack.Push(seged[0].ToString());
+                                    alkalmazottSzabalyok += matrixSzabalySeged[1];
+                                }
+                                else
+                                {
+                                    stack.Push(matrixSzabalySeged[0]);
+                                    alkalmazottSzabalyok += matrixSzabalySeged[1];
+                                }
+
+                                break;
+                            case 3:
+                                stack.Push(matrixSzabalySeged[1]);
+                                stack.Push(matrixSzabalySeged[0]);
+                                alkalmazottSzabalyok += matrixSzabalySeged[2];
+                                break;
+                            case 4:
+                                stack.Push(matrixSzabalySeged[2]);
+                                stack.Push(matrixSzabalySeged[1]);
+                                stack.Push(matrixSzabalySeged[0]);
+                                alkalmazottSzabalyok += matrixSzabalySeged[3];
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    
+                }
+                else if (this.szabalyMatrix[sorIndex, oszlopIndex].Contains("pop"))
+                {
+                    Console.WriteLine("Pop művelet");
+                    karakterPozicio++;
+                }
+                else if (this.szabalyMatrix[sorIndex, oszlopIndex].Contains("elfogad"))
+                {
+                    elfogad = true;
+                    eredmeny = "Az automata elfogadta a beírt stringet!";
+                }
+                else if (this.szabalyMatrix[sorIndex, oszlopIndex].Contains("X"))
+                {
+                    error = true;
+                    eredmeny = "Az automata nem fogadta el a beírt stringet.";
+                }
+                cikluslépteto++;
+            }
+
+            return eredmeny + " a " + cikluslépteto + " körben áll le.";
+        }
+
+    }
+}
---
+++ VeremAutomataBeadando/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Data;
+
+namespace VeremAutomataBeadando
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string[,] matrix = new string[12,7];
+        public MainWindow()
+        {
+            InitializeComponent();
+            string path = "C:\\Users\\ocbto\\source\\repos\\VeremAutomataBeadando\\VeremAutomataBeadando\\bin\\Debug\\rules.txt";
+            StreamReader sr = new StreamReader(path,Encoding.UTF8);
+            int n = 0;
+            while (!sr.EndOfStream)
+            {
+                string[] sor = sr.ReadLine().Split(';');
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[n, j] = sor[j];
+                }
+                n++;
+            }
+            sr.Close();
+            DataTable dt = ConvertMatrixToDataTable(matrix);
+
+            matrixGrid.ItemsSource = dt.DefaultView;
+        }
+        private DataTable ConvertMatrixToDataTable(string[,] matrix)
+        {
+            DataTable dt = new DataTable();
+
+
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                dt.Columns.Add();
+            }
+
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                DataRow row = dt.NewRow();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    row[j] = matrix[i, j];
+                }
+                dt.Rows.Add(row);
+            }
+
+            return dt;
+        }
+        private void Elemez(object sender, RoutedEventArgs e)
+        {
+            string input = inputString.Text;
+            Automata a = new Automata(input,matrix);
+            string output = a.Elemzes();
+            kiir.Text = output;
+        }
+    }
+}
